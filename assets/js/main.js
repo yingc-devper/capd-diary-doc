35b5dea78c9d930fa9d510bec279907a9c9e8dc9
yingclin <yingclin@hotmail.com>
2019-12-31 11:08:17 +0800

    add 服務條款.

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,5 +27,18 @@ $(document).ready(function() {
         $(this).ekkoLightbox();
     });    
 
-
+    if(isFromApp()) {
+        $(".noapp").hide();
+    }
 });
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+function isFromApp() {
+    return getUrlParameter("app") === "true";
+}
